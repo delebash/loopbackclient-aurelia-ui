a5e6b12ea36390e973bc1bac22c112ca603c50db
dan <danelebash@hotmail.com>
2017-08-18 06:12:38 -0400

    menu fixed

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 https://adarshpastakia.github.io/aurelia-ui-framework/#/
+
+export class App {
+
+  configureRouter(config, router) {
+    config.title = 'Contacts';
+    config.map([
+      {route: "", redirect: "view"},
+      {route: "view", name: "view", moduleId: "view", nav: true, title: "view"}
+    ]);
+  }
+}
+
+   this.router.navigateToRoute('view');
--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
+import {Router} from 'aurelia-router'
+import {inject} from 'aurelia-framework'
+
+@inject(Router)
 export class App {
 
   configureRouter(config, router) {
+
     config.title = 'Contacts';
     config.map([
-      {route: "", redirect: "view"},
-      {route: "view", name: "view", moduleId: "view", nav: true, title: "view"}
+      {route: ['', "welcome"], name: "welcome", moduleId: "welcome", nav: true, title: "Welcome"},
+      {route: "page-view", name: "page-view", moduleId: "page-view", nav: true, title: "My App"}
     ]);
+  }
+
+  constructor(router) {
+    this.router = router
   }
 }
